90de29f18d53c1374181498da22f87044da63fff
RithyTep <rithy.tep@techbodia.com>
2026-01-21 10:18:36 +0700

    lazyhand web

--- app/lazyhand/page.tsx
+++ app/lazyhand/page.tsx
@@ -3,8 +3,8 @@ import LazyhandLogin from "./lazyhand-login"
 import LazyhandShell from "./lazyhand-shell"
 import { LAZYHAND_AUTH_COOKIE, isLazyhandAuthed } from "@/lib/lazyhand-auth"
 
-export default function LazyhandPage() {
-	const cookieStore = cookies()
+export default async function LazyhandPage() {
+	const cookieStore = await cookies()
 	const authCookie = cookieStore.get(LAZYHAND_AUTH_COOKIE)?.value
 	const authed = isLazyhandAuthed(authCookie)
 
